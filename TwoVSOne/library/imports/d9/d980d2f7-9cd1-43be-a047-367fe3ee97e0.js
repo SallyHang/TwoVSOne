6197db96488151a68fa9f3eaedafd1ae98e30300
SallyZhang <38800064+SallyHang@users.noreply.github.com>
2018-07-11 16:44:28 +0800

    将“获取点击的棋子节点”部分独立成一个函数

--- TwoVSOne/library/imports/d9/d980d2f7-9cd1-43be-a047-367fe3ee97e0.js
+++ TwoVSOne/library/imports/d9/d980d2f7-9cd1-43be-a047-367fe3ee97e0.js
@@ -4,7 +4,7 @@ cc._RF.push(module, 'd980dL3nNFDvqBHNn/j7pfg', 'chessinit');
 
 'use strict';
 
-cc.Class({
+var Game = cc.Class({
     extends: cc.Component,
 
     properties: {
@@ -17,12 +17,32 @@ cc.Class({
             type: cc.Prefab
         }
     },
+    //返回点击的棋子节点
+    selectchess: function selectchess(event, chessList) {
+        var distance = [];
+        for (var i = 0; i < 25; i++) {
+            //计算鼠标点击位置和各棋子节点之间的距离
+            //将棋子节点的坐标转化为世界坐标系
+            var d = cc.pDistance(cc.v2(event.getLocationX(), event.getLocationY()), chessList[i + 1].convertToWorldSpaceAR(cc.v2(0, 0)));
+            distance.push(d);
+        };
+        var min = distance[0];
+        var minpos = 0;
+        //取距离最近的下标，即棋子节点位置
+        for (var j = 1; j < 25; j++) {
+            if (distance[j] < min) {
+                min = distance[j];
+                minpos = j;
+            }
+        }
+
+        return minpos;
+    },
     chessinit: function chessinit() {
         var self = this;
         var chessList = [cc.Node]; //棋子从下标1开始           
         for (var y = 0; y < 5; y++) {
             for (var x = 0; x < 5; x++) {
-                //****start 获取鼠标点击的棋子节点
                 var newNode = cc.instantiate(this.chessPrefab); //复制Chess预制资源
                 this.node.addChild(newNode);
                 newNode.setPosition(cc.p(x * (this.node.width / 4), y * (this.node.height / 4))); //根据棋盘和棋子大小计算使每个棋子节点位于指定位置
@@ -30,26 +50,12 @@ cc.Class({
                 newNode.opacity = 0;
                 chessList.push(newNode);
                 newNode.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
-                    var distance = [];
-                    for (var i = 0; i < 25; i++) {
-                        //计算鼠标点击位置和各棋子节点之间的距离
-                        //将棋子节点的坐标转化为世界坐标系
-                        var d = cc.pDistance(cc.v2(event.getLocationX(), event.getLocationY()), chessList[i + 1].convertToWorldSpaceAR(cc.v2(0, 0)));
-                        distance.push(d);
-                    };
-                    var min = distance[0];
-                    var minpos = 0;
-                    //取距离最近的下标，即棋子节点位置
-                    for (var j = 1; j < 25; j++) {
-                        if (distance[j] < min) {
-                            min = distance[j];
-                            minpos = j;
-                        }
-                    }
-                    //***** end 返回minpos
-                    //显示此棋子节点
-                    if (chessList[minpos + 1].getComponent(cc.Sprite).spriteFrame != null) {
-                        chessList[minpos + 1].getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(cc.url.raw('resources/clickwhite.png'));
+                    //为了调用类里面的静态方法，需要将类实例化
+                    var game = new Game();
+                    var chessNode = game.selectchess(event, chessList);
+                    //显示此棋子节点 节点从1开始 所以需要+1
+                    if (chessList[chessNode + 1].getComponent(cc.Sprite).spriteFrame != null) {
+                        chessList[chessNode + 1].getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(cc.url.raw('resources/clickwhite.png'));
                     }
                 });
             };
