adb4c4367ea695b62263c97d66f01d88436ae39a
SallyZhang <38800064+SallyHang@users.noreply.github.com>
2018-07-30 11:10:13 +0800

    the first product

--- TwoVSOne/library/imports/d9/d980d2f7-9cd1-43be-a047-367fe3ee97e0.js
+++ TwoVSOne/library/imports/d9/d980d2f7-9cd1-43be-a047-367fe3ee97e0.js
@@ -32,6 +32,21 @@ var Game = cc.Class({
         },
         directionY: { //Y轴
             default: []
+        },
+        whitenum: { //白色棋子颜色
+            default: 5
+        },
+        blacknum: { //黑色棋子颜色
+            default: 5
+        },
+        //结束页面
+        overSprite: {
+            default: null,
+            type: cc.Sprite
+        },
+        overLabel: {
+            default: null,
+            type: cc.Label
         }
     },
     //构造函数
@@ -73,6 +88,11 @@ var Game = cc.Class({
         if (game.occupiedcolor[theThree[0]] == game.occupiedcolor[theThree[1]]) {
             if (game.occupiedcolor[theThree[1]] != game.occupiedcolor[theThree[2]]) {
                 chessList[theThree[2]].opacity = 0;
+                if (game.occupiedcolor[theThree[2]] == 1) {
+                    game.whitenum--;
+                } else {
+                    game.blacknum--;
+                }
                 game.occupiedcolor[theThree[2]] = 0;
                 for (var k = 0; k < game.occupied.length; k++) {
                     if (game.occupied[k] == theThree[2]) {
@@ -83,6 +103,11 @@ var Game = cc.Class({
         } else {
             if (game.occupiedcolor[theThree[1]] == game.occupiedcolor[theThree[2]]) {
                 chessList[theThree[0]].opacity = 0;
+                if (game.occupiedcolor[theThree[0]] == 1) {
+                    game.whitenum--;
+                } else {
+                    game.blacknum--;
+                }
                 game.occupiedcolor[theThree[0]] = 0;
                 for (var k = 0; k < game.occupied.length; k++) {
                     if (game.occupied[k] == theThree[0]) {
@@ -100,25 +125,20 @@ var Game = cc.Class({
         var temp = chessNode;
         var s = parseInt((chessNode - 1) / 5); //取除数用于判断x轴棋子所在范围
         var r = chessNode % 5 == 0 ? 5 : chessNode % 5; //取余数用于判断y轴棋子所在范围
-        console.log(game.occupied);
-        console.log(temp + ' ' + s + ' ' + r);
         //X轴 j=0 X右侧；j=1 X左侧
         for (var j = 0; j < 2; j++) {
             //flag = true;
             temp = temp + game.directionX[j];
             while (temp > s * 5 && temp <= (s + 1) * 5) {
-                console.log('temp:' + temp);
                 if (game.occupied.indexOf(temp) != -1) {
                     count++;
                     temp = temp + game.directionX[j];
-                    console.log('countX:' + count);
                 } else {
                     temp = temp + game.directionX[j];
                 }
             }
             temp = chessNode;
         }
-        console.log('count:' + count);
         if (count == 3) {
             //判断这三个棋子是否连续
             var theThree = [];
@@ -141,7 +161,6 @@ var Game = cc.Class({
                 }
                 temp = chessNode;
             }
-            console.log('count3y:' + count);
             if (count == 3) {
                 //重新排列这三个连续的棋子，将颜色不同的那个吃掉
                 game.sortchesss(game, chessList, theThree);
@@ -163,7 +182,6 @@ var Game = cc.Class({
             }
             temp = chessNode;
         }
-        console.log('county:' + count);
         if (count == 3) {
             //判断这三个棋子是否连续
             var theThree = [];
@@ -190,7 +208,23 @@ var Game = cc.Class({
             }
         }
     },
+    b_oncemore: function b_oncemore() {
+        cc.director.loadScene("game"); //切换场景到结束场景
+    },
+    judgeover: function judgeover(game, self) {
+        if (game.whitenum == 1) {
+            self.overLabel.string = "黑子获胜";
+            self.overSprite.node.x = 0;
+            game.gameState = 'over';
+        }
+        if (game.blacknum == 1) {
+            self.overLabel.string = "白子获胜";
+            self.overSprite.node.x = 0;
+            game.gameState = 'over';
+        }
+    },
     chessinit: function chessinit() {
+        this.overSprite.node.x = 10000; //让结束画面位于屏幕外
         var self = this;
         var chessList = [cc.Node]; //棋子从下标1开始           
         for (var y = 0; y < 5; y++) {
@@ -205,19 +239,19 @@ var Game = cc.Class({
                 chessList.push(newNode);
                 newNode.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
                     console.log(game.gameState);
+                    console.log(game.prepare);
                     //显示此棋子节点 节点从1开始 所以需要+1
                     var chessNode = game.selectchess(event, chessList) + 1;
-                    if (game.occupied.indexOf(chessNode) == -1) {
-                        game.occupied.push(chessNode);
-                    }
-                    //console.log(game.occupied);
-                    //点击棋子变为半透明状态
-                    if (chessList[chessNode].opacity != 0) {
+                    //点击棋子变为半透明状态 若已经存在半透明棋子则不能再将其他棋子变为半透明
+                    if (chessList[chessNode].opacity != 0 && game.prepare == 0 && game.gameState != 'over') {
                         chessList[chessNode].opacity = 128;
                         game.prepare = 1;
                         game.lastNode = chessNode;
+                        //cc.director.loadScene("game");//切换场景到结束场景
                     } else {
-                        if (game.prepare == 1) {
+                        console.log(game.occupied);
+                        console.log(game.occupied.indexOf(chessNode));
+                        if (game.prepare == 1 && game.occupied.indexOf(chessNode) == -1 && game.gameState != 'over') {
                             if (game.lastNode != 0) {
                                 //判断不能跨棋子移动
                                 if (chessList[chessNode].position.x == chessList[game.lastNode].position.x) {
@@ -227,6 +261,7 @@ var Game = cc.Class({
                                         if (game.occupied.indexOf(i) != -1) {
                                             alert("不能跨越其他棋子！");
                                             chessList[game.lastNode].opacity = 255;
+                                            game.prepare = 0;
                                             return false;
                                         }
                                     }
@@ -238,22 +273,28 @@ var Game = cc.Class({
                                         if (game.occupied.indexOf(i) != -1) {
                                             alert("不能跨越其他棋子！");
                                             chessList[game.lastNode].opacity = 255;
+                                            game.prepare = 0;
                                             return false;
                                         }
                                     }
                                 }
                                 if (chessList[chessNode].position.x != chessList[game.lastNode].position.x && chessList[chessNode].position.y != chessList[game.lastNode].position.y) {
                                     alert("不能沿斜线移动棋子！");
+                                    chessList[game.lastNode].opacity = 255;
+                                    game.prepare = 0;
                                     return false;
                                 }
                             }
                             //目标棋子节点显示，上一棋子节点透明
-                            if (game.gameState == 'white') {
+                            console.log(game.occupiedcolor[chessNode]);
+                            if (game.gameState == 'white' && game.occupiedcolor[game.lastNode] == 1) {
                                 chessList[chessNode].getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(cc.url.raw('resources/whitechess.png'));
                                 chessList[chessNode].opacity = 255;
                                 chessList[game.lastNode].opacity = 0;
                                 game.occupiedcolor[chessNode] = 1;
-                                //console.log(game.lastNode);
+                                if (game.occupied.indexOf(chessNode) == -1) {
+                                    game.occupied.push(chessNode);
+                                }
                                 //将颜色列表中上一棋子颜色改为0
                                 game.occupiedcolor[game.lastNode] = 0;
                                 //将上一棋子节点从occupied中删除
@@ -263,15 +304,19 @@ var Game = cc.Class({
                                     }
                                 }
                                 game.prepare = 0;
-                                console.log(game.occupiedcolor);
                                 //判断吃子
                                 game.eatchess(chessNode, game, chessList);
                                 game.gameState = 'black';
-                            } else {
+                                //判断胜负
+                                game.judgeover(game, self);
+                            } else if (game.gameState == 'black' && game.occupiedcolor[game.lastNode] == 2) {
                                 chessList[chessNode].getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(cc.url.raw('resources/blackchess.png'));
                                 chessList[chessNode].opacity = 255;
                                 chessList[game.lastNode].opacity = 0;
                                 game.occupiedcolor[chessNode] = 2;
+                                if (game.occupied.indexOf(chessNode) == -1) {
+                                    game.occupied.push(chessNode);
+                                }
                                 game.occupiedcolor[game.lastNode] = 0;
                                 for (var k = 0; k < game.occupied.length; k++) {
                                     if (game.occupied[k] == game.lastNode) {
@@ -282,7 +327,16 @@ var Game = cc.Class({
                                 //判断吃子
                                 game.eatchess(chessNode, game, chessList);
                                 game.gameState = 'white';
+                                //判断胜负
+                                game.judgeover(game, self);
+                            } else {
+                                alert("此轮为对方执棋");
+                                chessList[game.lastNode].opacity = 255;
+                                game.prepare = 0;
+                                return false;
                             }
+                        } else if (game.occupied.indexOf(chessNode) == -1) {
+                            game.prepare = 0;
                         }
                     }
                 });
